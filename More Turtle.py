00fc049ffcee3abe316f38b5ed9e50e1837be3ae
offlight1 <100810134+offlight1@users.noreply.github.com>
2022-03-18 21:04:29 -0400

    You can use this to help you.
    Looking over this code can teach you the basics of Turtle. It shows you how to set a shape, color ,how to move the character, and how to use the pen.

---
+++ More Turtle.py
@@ -0,0 +1,32 @@
+
+import turtle
+while True:
+ kevin = turtle.Turtle()
+ kevin.color('green')
+ kevin.shape('turtle')
+ kevin.pensize = 7
+ kevin.forward(200)
+ kevin.left(90)
+ kevin.forward(200)
+ kevin.left(90)
+ kevin.forward(200)
+ kevin.left(90)
+ kevin.forward(200)
+ kevin.penup
+ kevin.left(200)
+ kevin.left(200)
+ kevin.left(90)
+ kevin.forward(100)
+ kevin.pendown
+ kevin.right(90)
+ kevin.forward(100)
+ kevin.left(160)
+ kevin.forward(200)
+ kevin.right(100)
+ kevin.forward(50)
+ kevin.beginfill
+ kevin.begin_fill
+ kevin.forward(50)
+ kevin.forward(25)
+ kevin.forward(5)
+
---
+++ Turtle Graphic Drawings.py
@@ -0,0 +1,45 @@
+
+import turtle
+
+kevin = turtle.Turtle()
+kevin.color('red')
+kevin.shape('turtle')
+
+
+kevin = turtle.Turtle()
+import turtle
+kevin = turtle.Turtle()
+kevin.color('red')
+kevin.shape('turtle')
+kevin.pensize = 5
+kevin.forward(100)
+kevin.right(90)
+kevin.forward(100)
+kevin.left(90)
+kevin.forward(1000)
+
+
+
+kevin.backward(1000)
+kevin.right(90)
+kevin.forward(100)
+kevin.right(45)
+kevin.forward(200)
+kevin.right(45)
+kevin.right(90)
+kevin.forward(500)
+
+kevin.right(90)
+kevin.forward(10)
+kevin.forward(5)
+kevin.forward(5)
+kevin.forward(5)
+kevin.right(90)
+kevin.forward(100)
+kevin.forward(25)
+kevin.forward(10)
+kevin.forward(10)
+kevin.forward(10)
+kevin.left(90)
+kevin.forward(5)
+kevin.forward(5)
